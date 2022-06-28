ab4ffcd4f5ee9767e9d75f569e5d78487a136bfc
Phat menghor <108237296+Phatmenghor@users.noreply.github.com>
2022-06-28 16:48:51 +0700

    Firth Push Java

--- Me.java
+++ Me.java
@@ -1,7 +1,7 @@
 package TestForMyFreeTime;
 
-public class HelloWorld {
+public class Me {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("My name is Phat menghor");
     }
 }
